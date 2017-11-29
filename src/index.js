f22e44ae798c7444098027001ee32686706585cb
Nebras <nebras.z.othman@gmail.com>
2017-11-29 14:43:31 +0100

    adding carousel.js and add categories

--- src/index.js
+++ src/index.js
@@ -2,45 +2,7 @@ import $ from 'jquery';
 import 'bootstrap/js/src';
 import './styles.scss';
 import navbarTemplate from './templates/navbar.html';
-import carouselTemplate from './templates/carousel.html';
-import carouselItemTeplate from './templates/carousel-item.html';
-
-
-function mkIndicator(number) {
-  return $(`<li data-target="#carousel-indicators" data-slide-to="${number}"></li>`);
-}
-function mkSlide(item) {
-  const $el = $(carouselItemTeplate);
-  $el.find('h2').text(item.name);
-  return $el;
-}
-
-function mkCarousel(items) {
-  // we create a jQuery object
-  const $el = $(carouselTemplate);
-  // we create a reference to the elements in which we want to put things
-  const $indicators = $el.find('.carousel-indicators');
-  const $slides = $el.find('.carousel-inner');
-
-  items.forEach((item, number) => {
-    const $indicator = mkIndicator(number);
-    const $slide = mkSlide(item);
-
-    if (number === 0) {
-      $slide.addClass('active');
-      $indicator.addClass('active');
-    }
-
-    $indicators.append($indicator);
-    $slides.append($slide);
-  });
-  return $el;
-}
-
-const catogories = [
-  { name: 'First' },
-  { name: 'Second' },
-];
+import mkCarousel from './carousel';
 /**
  * Carousel
  */
@@ -50,9 +12,13 @@ const catogories = [
  * Products
  */
 $(() => {
-  const $carousel = mkCarousel(catogories);
+  $.ajax('./static/categories.json')
+    .done((categories) => {
+      const $carousel = mkCarousel(categories);
+      $('#root').append($carousel);
+      $carousel.carousel();
+    });
+
   $('#root')
-    .append(navbarTemplate)
-    .append($carousel);
-  $('.carousel').carousel();
+    .append(navbarTemplate);
 });
